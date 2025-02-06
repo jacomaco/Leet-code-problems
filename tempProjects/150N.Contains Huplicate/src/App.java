9cffb4de59f94f4243a698e3f81965378cef035e
JacobJohansson <jacobknuts@gmail.com>
2025-02-06 01:06:19 +0100

    lärt mig att man inte ska ha import... i Neetcode

--- tempProjects/150N.Contains Huplicate/src/App.java
+++ tempProjects/150N.Contains Huplicate/src/App.java
@@ -3,11 +3,16 @@ public class App {
         Solution solution = new Solution();
         int[] numsNoDuplicates = new int[10];
         int[] nums = {1,2,3,3};
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             
             numsNoDuplicates[i] = i;
         }
         System.out.println(solution.hasDuplicate(nums));
         System.out.println(solution.hasDuplicate(numsNoDuplicates));
+        
+        for (int i : numsNoDuplicates) {
+            System.out.print(String.valueOf(i));
+        }
+        System.out.println();
     }
 }
--- tempProjects/150N.Contains Huplicate/src/Solution.java
+++ tempProjects/150N.Contains Huplicate/src/Solution.java
@@ -1,33 +1,14 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        int lengthOfNUms = nums.length;
-        sort(nums);
-        
-        for (int i = 0; i < lengthOfNUms - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int iterable : nums) {
+            if (set.contains(iterable)) {
                 return true;
             }
+            set.add(iterable);
         }
         return false;
-    }
-    public static int[] sort(int[] nums) {
-        int n = nums.length;
-        boolean swapped;
-        int temp;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (nums[j] < nums[j + 1] ) {
-                    temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            if (!swapped) {
-                break;
-            }
-        }
-        return nums;
     }
 }
