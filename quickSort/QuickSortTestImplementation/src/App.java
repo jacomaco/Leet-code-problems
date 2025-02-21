3b714599baf96de242cdb0aebd014b1594bec975
JacobJohansson <jacobknuts@gmail.com>
2025-02-21 02:47:21 +0100

    add QuickSort implementation using Lomuto scheme

---
+++ quickSort/QuickSortTestImplementation/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        QuickSort quickSort = new QuickSort();
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        quickSort.quickSort(arr);
+        
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
