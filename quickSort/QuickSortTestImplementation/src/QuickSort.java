753f339045619ff60005ad6a5e2f12051fd30a1a
JacobJohansson <jacobknuts@gmail.com>
2025-02-21 03:03:06 +0100

    fix: update quickSort to handle equal elements correctly in partitioning

--- quickSort/QuickSortTestImplementation/src/QuickSort.java
+++ quickSort/QuickSortTestImplementation/src/QuickSort.java
@@ -8,20 +8,19 @@ public class QuickSort {
     }
 
     private void quickSort(int[] array, int low, int high) {
-        if (low < high) {
+        if (low <= high) {
             int pivotIndex = partition(array, low, high);
             quickSort(array, low, pivotIndex - 1);
             quickSort(array, pivotIndex + 1, high);
         }
     }
 
-    private int partition(int[] array, int low, int high) {
-        int pivot = array[high];
+    private int partition(int[] array, int low, int high) { // Lomuto scheme
+        int pivot = array[high]; // One of many ways to choose a pivot
         int i = low - 1;
-        for (int j = low; j < high; j++) {
+        for (int j = low; j < high; j++) { // j itterates though all exempt the last element in the array
             if (array[j] <= pivot) {
-                i++;
-                swap(array, i, j);
+                swap(array, ++i, j);
             }
         }
         swap(array, high, i + 1);
